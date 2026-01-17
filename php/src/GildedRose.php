730f47da66ee43c5049f67ec231660dcefe6adea
janwillemvisser <jw.visser@novict.com>
2026-01-17 17:26:19 +0100

    Refactor code and add tests

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -16,52 +16,76 @@ final class GildedRose
 
     public function updateQuality(): void
     {
-        foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+        foreach ($this->items as $key => $item) {
+            switch ($item->name) {
+                case 'Aged Brie':
+                    $this->updateAgedBrie($this->items[$key]);
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $this->updateBackstagePasses($this->items[$key]);
+                    break;
+                case str_contains($item->name, 'Conjured'):
+                    $this->updateConjured($this->items[$key]);
+                    break;
+                case 'Sulfuras, Hand of Ragnaros':
+                    break;
+                default:
+                    $this->updateNormalItem($this->items[$key]);
+                    break;
             }
         }
+    }
+
+    private function updateAgedBrie($item): void
+    {
+        $increase = 1;
+
+        if ($item->sellIn < 0) {
+            $increase++;
+        }
+
+        $item->quality = min(50, $item->quality + $increase);
+    }
+
+    private function updateBackstagePasses($item): void
+    {
+        if ($item->sellIn < 0) {
+            $item->quality = 0;
+            return;
+        }
+
+        $increase = 1;
+
+        if ($item->sellIn < 11) {
+            $increase++;
+        }
+
+        if ($item->sellIn < 6) {
+            $increase++;
+        }
+
+        $item->quality = min(50, $item->quality + $increase);
+    }
+
+    private function updateConjured($item): void
+    {
+        $decrease = 2;
+
+        if ($item->sellIn < 0) {
+            $decrease += 2;
+        }
+
+        $item->quality = max(0, $item->quality - $decrease);
+    }
+
+    private function updateNormalItem($item): void
+    {
+        $decrease = 1;
+
+        if ($item->sellIn < 0) {
+            $decrease++;
+        }
+
+        $item->quality = max(0, $item->quality - $decrease);
     }
 }
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -15,6 +15,110 @@ class GildedRoseTest extends TestCase
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame('foo', $items[0]->name);
+    }
+
+    public function testNoNegativeValues(): void
+    {
+        $items = [new Item('Camera', 50, 2)];
+        $gildedRose = new GildedRose($items);
+
+        for ($day = 1; $day < 3; $day++) {
+            $gildedRose->updateQuality();
+        }
+
+        $this->assertGreaterThanOrEqual(
+            '0',
+            $items[0]->quality,
+            'Quality should never be negative'
+        );
+    }
+
+    public function testAgedBrie(): void
+    {
+        $items = [new Item('Aged Brie', 50, 2)];
+        $gildedRose = new GildedRose($items);
+
+        for ($day = 1; $day < 3; $day++) {
+            $previousQuality = $items[0]->quality;
+            $gildedRose->updateQuality();
+            $this->assertSame(
+                $previousQuality + 1,
+                $items[0]->quality,
+                'Aged Brie quality should increase'
+            );
+        }
+    }
+
+    public function testMaxQuality(): void
+    {
+        $items = [new Item('Aged Brie', 50, 48)];
+        $gildedRose = new GildedRose($items);
+
+        for ($day = 1; $day < 3; $day++) {
+            $gildedRose->updateQuality();
+        }
+        $this->assertLessThanOrEqual(
+            50,
+            $items[0]->quality,
+            'Quality should never be greater than 50'
+        );
+    }
+
+    public function testSulfurasQuality(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 0, 80)];
+        $gildedRose = new GildedRose($items);
+
+        for ($day = 1; $day < 3; $day++) {
+            $gildedRose->updateQuality();
+            $this->assertSame(
+                80,
+                $items[0]->quality,
+                'Sulfuras quality should never change'
+            );
+        }
+    }
+
+    public function testBackstagePass(): void
+    {
+        $items = [
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 25),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 25),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 25),
+        ];
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->updateQuality();
+        $this->assertSame(
+            26,
+            $items[0]->quality,
+            'Backstage pass quality should increase by 1 when there are more than 10 days'
+        );
+        $this->assertSame(
+            27,
+            $items[1]->quality,
+            'Backstage pass quality should increase by 2 when there are 10 days or less'
+        );
+        $this->assertSame(
+            28,
+            $items[2]->quality,
+            'Backstage pass quality should increase by 3 when there are 5 days or less'
+        );
+    }
+
+    public function testConjured(): void
+    {
+        $items = [new Item('Conjured Mana Cake', 15, 40)];
+        $gildedRose = new GildedRose($items);
+
+        for ($day = 1; $day <= 3; $day++) {
+            $gildedRose->updateQuality();
+        }
+        $this->assertSame(
+            34,
+            $items[0]->quality,
+            'Conjured quality should decrease by 2'
+        );
     }
 }
